b830747883556f9ee64efc7e35c23d4092498195
hydrz <n.haoyuan@gmail.com>
2025-05-24 01:59:42 +0800

    更新 Navicat Premium 的 sha256 校验和

--- Casks/navicat-premium-1726.rb
+++ Casks/navicat-premium-1726.rb
@@ -1,6 +1,6 @@
 cask "navicat-premium-1726" do
   version "17.2.6"
-  sha256 "46ed154fadcbff90f06147ecee24106a59a2bf0953565370dacd850b3e162095"
+  sha256 "7872e35621d9c87763ab175b72a259cc6c13ddc8c44c16b0246934b02416945e"
 
   url "https://pub-ba6dbd1d80b247e8bc4a9fd9e197bac0.r2.dev/apps/darwin/navicat-premium-#{version}.dmg"
   name "Navicat Premium"
