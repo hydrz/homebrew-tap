ce821f05b218725eda38c5c2e57549edbc491c8b
hydrz <n.haoyuan@gmail.com>
2025-12-13 21:25:01 +0800

    chore: remove macOS version dependency for navicat-premium and tg-pro

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -14,7 +14,6 @@ cask "navicat-premium" do
   end
 
   conflicts_with cask: "navicat-premium@15"
-  depends_on macos: ">= :big_sur"
 
   app "Navicat Premium.app"
 
--- Casks/tg-pro.rb
+++ Casks/tg-pro.rb
@@ -14,7 +14,6 @@ cask "tg-pro" do
   end
 
   auto_updates true
-  depends_on macos: ">= :high_sierra"
 
   app "TG Pro.app"
 
